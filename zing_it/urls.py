a224030fe925f484e41842d485cb17c9573c5f50
dbanerji <deboshruti.banerji95@gmail.com>
2023-10-16 13:32:10 +0100

    Creates song playlist using template inheritance and static image

--- zing_it/urls.py
+++ zing_it/urls.py
@@ -4,4 +4,5 @@ from zing_it import views
 urlpatterns = [
     path('', views.home,name="home"),
     path('about/',views.about,name="about"),
+    path('playlist/<int:id>',views.playlist,name="playlist")
 ]
